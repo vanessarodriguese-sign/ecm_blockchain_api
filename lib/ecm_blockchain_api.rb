73b6887b0c7e590b846ca846cc25a63ac454a096
Wardy <office@dragonsdesign.co.uk>
2023-02-28 14:15:22 +0000

    initialised gem

---
+++ lib/ecm_blockchain_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ecm_blockchain_api/version"
+
+module EcmBlockchainApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ecm_blockchain_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe EcmBlockchainApi do
+  it "has a version number" do
+    expect(EcmBlockchainApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
